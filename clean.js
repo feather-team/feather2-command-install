67a98fb676df53f130d043ef27ed0f0e327d7eb4
zhanghao <jsyczhanghao@sina.com>
2016-08-19 20:19:14 +0800

    commit

---
+++ clean.js
@@ -0,0 +1,98 @@
+'use strict';
+
+var fs = require('fs');
+var _ = feather.util;
+var cleanList = /\.(?:txt|lock|md)|LICENSE|Grunt|gulp|Gemfile/i;
+var configFinds =['bower.json', 'component.json', 'package.json', '.bower.json'];
+var minReg = /[.-]min(?=\.)/;
+var normalize = require('path').normalize;
+
+function del(dir, file){
+    var name = file.substr(dir.length);
+
+    if(_.exists(file)){
+        console.log('    ' + _.pad(name, 60, ' ') + 'deleted'.red);
+        _.del(file);
+    }
+}
+
+module.exports = function(name){
+    var dir = normalize(process.cwd()) + '/components/' + name + '/';
+    var files = fs.readdirSync(dir);
+
+    if(files.length){
+        console.log('\nclean files:'.yellow);
+
+        var distDir = normalize(dir + 'dist/');
+        var hasDist = files.indexOf('dist') > -1 && _.isDir(distDir);
+
+        files.forEach(function(file){
+            if(
+                cleanList.test(file) 
+                || hasDist && file.indexOf('dist') == -1 && configFinds.indexOf(file) == -1
+            ){
+                del(dir, dir + file);
+                return;
+            }
+        });
+    
+        var config, json = {};
+
+        for(var i = 0; i < configFinds.length; i++){
+            var temp = dir + configFinds[i];
+
+            if(_.exists(temp)){
+                json = _.readJSON(temp);
+                config = temp;
+                break;
+            }
+        }
+
+        if(config){
+            var mains = _.makeArray(json.main), r = [];
+
+            for(var i = 0; i < mains.length; i++){
+                if(_.exists(dir + mains[i])){
+                    json.main = mains[i];
+                    break;
+                }
+            }
+
+            if(hasDist){
+                fs.renameSync(distDir, distDir = dir + '_dist_');
+                _.copy(distDir, dir);
+                _.del(distDir);              
+            }
+
+            var main = json.main;
+
+            if(typeof main == 'string'){
+                if(/(?:^|\/)dist\//.test(main)){
+                    main = main.replace(/(?:^|\/)dist\//, '');
+                }
+                
+                if(minReg.test(main)){
+                    var temp = main.replace(minReg, '');
+
+                    if(_.exists(dir + temp)){
+                        main = temp;
+                    }
+                }
+            }  
+
+            json.main = main;
+        }
+
+        //clean min
+        _.find(dir, minReg).forEach(function(file){
+            _.exists(file.replace(minReg, '')) && del(dir, file);
+        });
+
+        //clean source map
+        _.find(dir, /\.map/).forEach(function(file){
+            del(dir, file);
+        })
+
+        _.write(dir + 'bower.json', JSON.stringify(json, null, 2));
+    }
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+exports.name = 'install';
+exports.usage = 'install [options] <components>[@<version>]';
+exports.desc = 'install components';
+exports.options = {
+    '-F, --force-latest': 'Force latest version on conflict',
+    '-f, --force': 'If dependencies are installed, it reinstalls all installed components. It also forces installation even when there are non-bower directories with the same name in the components directory. Also bypasses the cache and overwrites to the cache anyway.',
+    '-h, --help': 'Show this help message'
+};
+
+var bower = require('bower');
+var clean = require('./clean.js');
+
+exports.run = function(argv, cli, env) {
+    if(argv.h || argv.help){
+        return cli.help(exports.usage, exports.options);
+    }
+
+    var pkgs = argv._.slice(1).map(function(item){
+        return item.toString().split('@').join('#');
+    });
+
+    try{
+        argv.directory = 'components';
+        bower.commands.install(pkgs, argv, argv).on('log', function(data){
+            var pkgName = data.data.endpoint.source + '@' + data.data.endpoint.target;
+            var message = [
+                'download',
+                feather.util.pad(pkgName, 20, ' ').green,
+                feather.util.pad(data.id, 15, ' ', true).blue,
+                data.message
+            ];
+
+            console.log(message.join('  '));
+        }).on('end', function(pkgs){
+            feather.util.map(pkgs, function(name, info){
+                console.log('\n' + name + '@' + info.pkgMeta.version + ' components/' + name);
+
+                var deps = [];
+
+                feather.util.map(info.dependencies || {}, function(name, info){
+                    deps.push(name + '@' + info.pkgMeta.version);
+                });
+
+                var max = deps.length - 1;
+
+                deps.forEach(function(dep, key){
+                    var b = key == max ? '└──' : '├──';
+                    console.log(b + ' ' + dep);
+                });
+
+                //clean 
+                clean(name);
+            });
+        });
+    }catch(e){};
+
+    process.on('uncaughtException', function(err){
+        feather.log.throw = false;
+        feather.log.error(err.message);
+    });
+};
